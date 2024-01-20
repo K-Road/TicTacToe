79672d781541e10d7ff73055969b41d72d6e7ea3
K-Road <game@chrodeprime.com>
2024-01-20 13:46:44 +1100

    added image load

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,10 +1,21 @@
+import tkinter as tk
+from PIL import Image, ImageTk
 from graphics import Line, Point
 from cell import Cell
 import time
 
-class TicTakToe:
-    def __init__(self):
-        pass
+class TicTacToe:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("Tic Tac Toe")
+
+        #image load
+        self.image_o = self._load_image("./images/o.png")
+        self.image_x = self._load_image("./images/x.jpg")
+        self.image_x2 = self._load_image("./images/x2.jpg")
+
+        #create cells here
+        #2D array 3 x 3 cells
 
     #Create cells for game
     def _create_cells(self):
@@ -20,3 +31,8 @@ class TicTakToe:
 
     def _animate(self):
         pass
+
+    def _load_image(self,filename):
+        image = Image.open(filename)
+        image = image.resize((50,50), Image.ANTIALIAS)
+        return ImageTk.PhotoImage(image)
